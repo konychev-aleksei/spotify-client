275e181ebdd9cd94ccb7b9ff28b52b2703bf7ef0
konychev-aleksei <91011728+konychev-aleksei@users.noreply.github.com>
2021-09-21 22:39:03 +0400

    Update New.js

--- src/components/major/New.js
+++ src/components/major/New.js
@@ -25,7 +25,7 @@ const New = ({ myPlaylists, setMyPlaylists }) => {
 
   const handlePlaylistCreation = async (e) => {
     e.preventDefault()
-    await api.createPlaylist(name, await cropImage(), 'konychevaleksei')
+    await api.createPlaylist(name, await cropImage(), userName)
     setMyPlaylists([...myPlaylists, name])
     history.push('/')
   }
